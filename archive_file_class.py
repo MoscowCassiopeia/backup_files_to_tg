9eeaef75312c8e52cbf0608cb2e4ebc2713f4370
MoscowCassiopeia <b2343@ya.ru>
2018-01-24 22:04:30 +0300

    commit

---
+++ archive_file_class.py
@@ -0,0 +1,81 @@
+import os
+import zipfile
+import re
+'''
+Класс архивирует папки рекурсивно с подпапками и файлами, а так же одиночные файлы.
+Позволяет указать файлы которые нужно исключить или включить, через регулярные выражения
+'''
+ZIP_LZMA = zipfile.ZIP_LZMA
+ZIP_DEFLATED = zipfile.ZIP_DEFLATED
+ZIP_BZIP2 = zipfile.ZIP_BZIP2
+ZIP_STORED = zipfile.ZIP_STORED
+
+
+class zip_arch:
+    def __init__(self, compress_type=ZIP_DEFLATED, include=None, exclude=None):
+        self.compress_type = compress_type
+        self.include = include
+        self.exclude = exclude
+
+    def compress(self, path_src, path_dst):
+        '''
+        Непосредственно архивирует рекурсивно папки и файлы, или одиночный файл
+        '''
+        dir_path = os.path.split(path_dst)[0]    
+        if not os.path.exists(path_src): 
+            raise FileNotFoundError(f'path {path_src} not exist')            
+        elif not os.path.exists(dir_path):
+            raise FileNotFoundError(f'path {dir_path} not exist')
+            
+        if os.path.isdir(path_src):
+            with zipfile.ZipFile(path_dst, 'w') as zip_obj:            
+                for folder, subdirs, files in os.walk(path_src):    
+                    for file in files:
+                        if self.filter(file):
+                            fname = os.path.join(folder, file)                            
+                            relpath = os.path.relpath(os.path.join(folder, file), os.path.split(path_src)[-1])                                                        
+                            zip_obj.write(filename=fname,
+                                          arcname=relpath,
+                                          compress_type=self.compress_type)
+        elif os.path.isfile(path_src):
+            with zipfile.ZipFile(path_dst, 'w') as zip_obj:            
+                zip_obj.write(filename=path_src,
+                              arcname=os.path.split(path_src)[-1],
+                              compress_type=self.compress_type)
+
+    def filter(self, fname):
+        '''
+        Возвращает True или False, для имени заданного файла.
+        В зависимости от установленных фильтров include и exclude
+        '''
+        if self.include and not self.exclude:            
+            return re.match(self.include, fname)
+        elif self.exclude and not self.include:
+            if re.match(self.exclude, fname):
+                return False
+            else:
+                return True
+        elif self.include and self.exclude:
+            raise Exception('Both exclude and include not possible use')
+            return False
+        else:
+            return True
+            
+
+if __name__ == '__main__':
+    #src = '/home/user/Documents'
+    src = '/home/shedar/Documents/BicycleProof'
+    dst = f'{src}.zip'
+
+    # включить только файлы с расширение *.txt и *.odt
+    # тип компрессии ZIP_LZMA, по умолчанию ZIP_DEFLATED
+    zip_obj = zip_arch(include='.*txt$|.*odt$', compress_type=zipfile.ZIP_LZMA)    
+    zip_obj.compress(src, dst)
+  
+
+
+   
+    
+
+
+
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import read_cfg_class
 import hash_class
 import send_file_class
+import archive_file_class
 import logging
 import sys
+import os
 
 CFG_FILE = sys.argv[1]
 
@@ -20,20 +22,85 @@ def main():
     
 
     for i, file in enumerate(cfg_dict['files']):
-        hash_fl = hash_obg.get_hash(file['path'])                
-        if hash_fl.lower() != file['hash'].lower():
-            logging.info(f'hash_old:{cfg_dict["files"][i]["hash"]}')
-            logging.info(f'hash_new:{hash_fl}')
-            cfg_dict['files'][i]['hash'] = hash_fl
-            logging.info(f'file_path_to_send:{file["path"]}')
-            try:
-                ret = send_obj.send_fl(file['path'])                                                
-                is_change = True
-            except Exception as e:
-                logging.info(f'Error send file:{file["path"]}')
+
+        # Если указанный путь это директория
+        if os.path.isdir(file['path']):
+            src = file['path']
+            dst = f'{src}.zip'
             
+            # установлен ли фильтр
+            if 'include' in file and file['include']:                
+                zip_obj = archive_file_class.zip_arch(include=file['include'],
+                                                          compress_type=archive_file_class.ZIP_LZMA)
+                zip_obj.compress(src, dst)
+            elif 'exclude' in file and file['exclude']:                
+                zip_obj = archive_file_class.zip_arch(exclude=file['exclude'],
+                                                          compress_type=archive_file_class.ZIP_LZMA)
+                zip_obj.compress(src, dst)
+            else:
+                zip_obj = archive_file_class.zip_arch(compress_type=archive_file_class.ZIP_LZMA)            
+                zip_obj.compress(src, dst)
+
+            # считаем хеш архива, это и будет хеш каталога
+            hash_dir = hash_obg.get_hash(dst)
+            # сравниваем хеш архива каталога с хешем который записан в файле настроек
+            if hash_dir.lower() != file['hash'].lower():
+                # меняем старый хеш на новый в конфиге
+                cfg_dict['files'][i]['hash'] = hash_dir
+                try:
+                    # Отправляем zip файл в телеграм
+                    send_obj.send_fl(dst)
+                    # флаг означающий что конфиг нужно перезаписать новыми данными                                         
+                    is_change = True
+                except Exception as e:
+                    logging.info(f'Error send file:{dst}')
+                else:
+                    # Удаляем отправленный файл
+                    logging.info(f'File {dst} send to tg')
+                    os.remove(dst)
+                    logging.info(f'File {dst} removed')
+
+        # Если указанный путь это файл
+        elif os.path.isfile(file['path']):
+            # считаем хеш файла и кладем в переменную
+            hash_fl = hash_obg.get_hash(file['path'])
+
+            # сравниваем хеш файла с хешем который записан в файле настроек               
+            if hash_fl.lower() != file['hash'].lower():
+                logging.info(f'hash_old:{cfg_dict["files"][i]["hash"]}')
+                logging.info(f'hash_new:{hash_fl}')
+                cfg_dict['files'][i]['hash'] = hash_fl
+                logging.info(f'file_path_to_send:{file["path"]}')
+
+                # Если указана опция архивировать, архивируем
+                if cfg_dict['archive'].lower() == 'true':
+                    zip_obj = archive_file_class.zip_arch(compress_type=archive_file_class.ZIP_LZMA)
+                    zip_obj.compress(file['path'], f'{file["path"]}.zip')
+                    
+                    try:
+                        # Отправляем zip файл в телеграм
+                        send_obj.send_fl(f'{file["path"]}.zip')
+                        # флаг означающий что конфиг нужно перезаписать новыми данными                                         
+                        is_change = True
+                    except Exception as e:
+                        logging.info(f'Error send file:{file["path"]}')
+                    else:
+                        # Удаляем отправленный файл
+                        os.remove(f'{file["path"]}.zip')
+
+                # Если не указана опция архивировать, просто отправляем файл в телеграм
+                else:
+                    try:
+                        send_obj.send_fl(file['path'])
+                        # флаг означающий что конфиг нужно перезаписать новыми данными                
+                        is_change = True
+                    except Exception as e:
+                        logging.info(f'Error send file:{file["path"]}')
+                
             
+    
     if is_change:
+        # запись конфига в файл
         cfg_obj.write_cfg(cfg_dict)
     else:
         logging.info('Not Change Files')
